2a61de2a18582a413c529e0eff18fdd77934ab97
PrathaMaharjan <pratha.maharjan03@gmail.com>
2026-09-03 12:18:58 +0545

    added db url

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,6 +3,6 @@ import { neon } from "@neondatabase/serverless";
 import * as schema from "./schema";
 
 const connectionString =
-  process.env.DATABASE_URL || "postgres://dummy:dummy@localhost:5432/dummy";
+  process.env.DATABASE_URL || "postgresql://neondb_owner:npg_cp4Mq0xkJBtn@ep-dark-heart-azl90vp0-pooler.c-3.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require";
 const sql = neon(connectionString);
 export const db = drizzle(sql, { schema });
